f4512bddb6cbaba9b4e1629ca97eba78082599e3
Shuraimi <92625656+Shuraimi@users.noreply.github.com>
2022-11-09 15:38:12 +0530

    Add files via upload

---
+++ Pattern20.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Pattern20{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number to print the pattern");
+        int n=sc.nextInt();
+        for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++)
+            System.out.print("*");
+            
+            for(int k=1;k<=((n-i)*2);k++)
+            System.out.print(" ");
+            
+            for(int l=1;l<=i;l++)
+            System.out.print("*");
+            
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++)
+            System.out.print("*");
+            
+            for(int k=1;k<=((n-i)*2);k++)
+            System.out.print(" ");
+            
+            for(int l=1;l<=i;l++)
+            System.out.print("*");
+            
+            System.out.println();
+        }
+    }
+}
