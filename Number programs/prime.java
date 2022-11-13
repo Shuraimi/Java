33e4ef41b23f90c93a05c07609fe0d2f02f7bc0a
Shuraimi <92625656+Shuraimi@users.noreply.github.com>
2022-11-13 19:23:12 +0530

    Added comment

--- Number programs/prime.java
+++ Number programs/prime.java
@@ -5,8 +5,8 @@ System.out.println("Enter your number");
 
 int n=sc.nextInt(); 
 int count=0;
-
-for(int i=2;i<=Math.sqrt(n);i++){
+//i can also be less than or equal to n
+for(int i=2;i<Math.sqrt(n);i++){
 
 if(n%i==0)
 count++;
