29bf9384c6d5a4dea941e6844462b5a624ada46e
Shuraimi <92625656+Shuraimi@users.noreply.github.com>
2022-11-17 09:09:21 +0530

    Update DecimalToBinaryArr.java

--- Conversions/DecimalToBinaryArr.java
+++ Conversions/DecimalToBinaryArr.java
@@ -1,6 +1,6 @@
 //using arrays
 import java.util.*;
-public class Main {
+public class DecimalToBinaryArr {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number ");
