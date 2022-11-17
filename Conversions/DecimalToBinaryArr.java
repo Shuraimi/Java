269b968f6dba0aadbeacaa3490540b7d8a966f21
Shuraimi <92625656+Shuraimi@users.noreply.github.com>
2022-11-17 09:08:41 +0530

    Create DecimalToBinaryArr.java

---
+++ Conversions/DecimalToBinaryArr.java
@@ -0,0 +1,21 @@
+//using arrays
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n = sc.nextInt();
+		int q;
+		int rem[] = new int[32]; //used array
+		int i = 0;
+		while (n != 0) {
+			rem[i] = n % 2;
+			i++;
+			n = n / 2;
+		}
+		//printing the array in reverse
+		for (int j = rem.length - 1; j >= 0; j--)
+			System.out.print(rem[j]);
+
+	}
+}
