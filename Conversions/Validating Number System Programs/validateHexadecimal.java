be9a311825045c6ae2a0d7f1eeb625e9e34c7624
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-11-18 09:07:27 +0530

    Create validateHexadecimal.java

---
+++ Conversions/Validating Number System Programs/validateHexadecimal.java
@@ -0,0 +1,36 @@
+//function to validate hexadecimal and return true if true
+import java.util.*;
+public class Main {
+	static boolean isHexa(String s) {
+		int j;
+		char ch;
+		boolean flag = false;
+		for (int i = 0; i < s.length(); i++) {
+			ch = s.charAt(i);
+			j = ch;
+			if (Character.isDigit(ch)) {
+
+				flag = true;
+			} else if (j >= 65 && j <= 70) {
+
+				flag = true;
+			} else {
+				flag = false;
+			}
+
+		}
+		return flag;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s;
+		boolean Hexa;
+		do {
+			System.out.println("Enter your string");
+			s = sc.nextLine();
+			Hexa = isHexa(s);
+		} while (!Hexa);
+		System.out.println(Hexa);
+
+	}
+}
