4f29556d5e14812bc3aa5f9fc84bde5267df21d5
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-11-18 09:19:13 +0530

    Create validateOctal.java

---
+++ Conversions/Validating Number System Programs/validateOctal.java
@@ -0,0 +1,27 @@
+//Program to validate whether a given number is ocatl or not
+import java.util.*;
+public class validateOctal {
+	static boolean isOctal(int N) {
+		int dig;
+		while (N != 0) {
+			dig = N % 10;
+			if (dig > 7)
+				return false;
+			N = N / 10;
+		}
+		return true;
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		boolean k;
+
+		do {
+			System.out.println("Enter you number");
+			n = sc.nextInt();
+			k = isOctal(n);
+		} while (!(k));
+		System.out.println(k);
+	}
+}
