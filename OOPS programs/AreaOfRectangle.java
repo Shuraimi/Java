295ddc3881731fe31293648704f92039cf645708
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-12-15 08:46:15 +0530

    Create AreaOfRectangle.java

---
+++ OOPS programs/AreaOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Rectangle
+{
+
+// Write your int length;
+int length;
+int breadth:
+int getArea(){
+ int area=length*breadth;
+ return area;
+
+}
+}
