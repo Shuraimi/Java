c1a9603df908ab90d29a70f3a80764eabdf02771
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-12-15 08:57:06 +0530

    Create PrintNameAndAge.java

---
+++ OOPS programs/PrintNameAndAge.java
@@ -0,0 +1,51 @@
+import java.util.*;
+ import java.io.*;
+
+class Person {
+
+//Complete the class 
+  private String name;
+
+private int age; 
+  void SetValue(String name, int age) {
+
+name=this.name; 
+    age=this.age;
+    }
+
+void GetValue(String name,int age){
+  System.out.println("The name of the person is "+name+" and the age is "+age+".");
+  }
+  }
+class PrintNameAndAge{
+  public static void main(String args[]){ {
+
+
+
+
+
+
+
+
+
+// Write your code here 
+  Scanner sc=new Scanner(System.in);
+
+String name-sc.nextLine(): 
+    int age=sc.nextInt();
+
+Person p=new Person();
+
+p.SetValue(name, age); 
+p.GetValue(name, age);
+    }
+  }
+
+
+
+
+
+
+
+
+
