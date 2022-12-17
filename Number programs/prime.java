7191634cd32668f76f43b2e66128a778cced4db5
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-12-17 19:55:55 +0530

    Update prime.java
    Added some lines to overcome errors

--- Number programs/prime.java
+++ Number programs/prime.java
@@ -4,17 +4,22 @@ public static void main(String[] args) { Scanner sc=new Scanner(System.in);
 System.out.println("Enter your number");
 
 int n=sc.nextInt(); 
+if(n<=1)
+System.out.println("The given number "+n+" is not prime number.");
 int count=0;
 //i can also be less than or equal to n
-for(int i=2;i<Math.sqrt(n);i++){
+//i*i<=n can be used
+for(int i=2;i<=Math.sqrt(n);i++){
 
 if(n%i==0)
 count++;
 }
-System.out.println(count);
+//System.out.println(count);
+if(n>1){
 if(count==0)
-System.out.println("Prime number");
+System.out.println("The given number "+n+" is Prime number.");
 else
-System.out.println("Not a Prime number");
+System.out.println("The given number "+n+" is not a Prime number.");
+}
 }
 }
