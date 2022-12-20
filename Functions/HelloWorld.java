923e41ecd87c700bf3b4b21e0b39b7c57eea72da
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2022-12-20 19:52:42 +0530

    Create HelloWorld.java
    Needs to be modified

---
+++ Functions/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+	
+	// Write your print function here
+    void print(){
+        System.out.println("Hello World");
+    }
+	
+	public static void main(String args[]) {
+		Solution obj = new Solution();
+		obj.print();
+	}
+}
